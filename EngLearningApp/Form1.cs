354ed7058c3259a8561ca600d26d22fe5dc171a1
Balint Sipos <sipos.balint9@gmail.com>
2018-09-08 23:27:05 +0200

    adding translator class: This method steal the hungarian word from a online translator website

--- EngLearningApp/Form1.cs
+++ EngLearningApp/Form1.cs
@@ -13,13 +13,16 @@ namespace EngLearningApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string filePath = @"C:\Users\sypy\Downloads\HarryPotterAndTheSorceresStone.pdf";
-            //string filePath = @"F:\Dev\videoCourses\Udemy - Spring Framework 5 Beginner to Guru\02 Building a Spring Boot Web App\009 Open Project in IntelliJ-subtitle-en.vtt";
-            FileReader fileReader = new FileReader();
-            TextFormatter textFormatter = new TextFormatter();
+            //string filePath = @"C:\Users\sypy\Downloads\HarryPotterAndTheSorceresStone.pdf";
+            ////string filePath = @"F:\Dev\videoCourses\Udemy - Spring Framework 5 Beginner to Guru\02 Building a Spring Boot Web App\009 Open Project in IntelliJ-subtitle-en.vtt";
+            //FileReader fileReader = new FileReader();
+            //TextFormatter textFormatter = new TextFormatter();
 
-            string fullText = fileReader.readPdfToString(filePath); 
-            List<string> wordSet = textFormatter.getWorldListFromString(fullText);
+            //string fullText = fileReader.readPdfToString(filePath); 
+            //List<string> wordSet = textFormatter.getWorldListFromString(fullText);
+
+            Translator d = new Translator();
+            d.EngToHun("apple");
         }
     }
 }
---
+++ EngLearningApp/Translator.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using System.Linq;
+
+namespace EngLearningApp
+{
+    class Translator
+    {
+        public string EngToHun(string eng)
+        {
+            string htmlPath_Front= @"http://www.szotar.sztaki.hu/search?searchWord=";
+            string htmlPath_End = @"&fromlang=eng&tolang=hun&outLanguage=hun";
+            string fullPath = htmlPath_Front + eng + htmlPath_End;
+
+            HtmlDocument doc = new HtmlWeb().Load(fullPath);
+
+            var itemList = doc.DocumentNode.SelectNodes("//*[@id='searchresults']/div[2]/div/div/div[2]/ol/li/div[2]/ol[1]/li/div/a").Select(p => p.InnerText).ToList();
+            string hun = itemList.FirstOrDefault().ToString();
+
+            return hun;
+        }
+    }
+}
