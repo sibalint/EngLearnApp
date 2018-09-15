eb20d902cf72854a8db5841e47d379e517ffe813
Balint Sipos <sipos.balint9@gmail.com>
2018-09-15 12:58:59 +0200

    improve removeFromList affix method

--- EngLearningApp/service/RemoveFromList.cs
+++ EngLearningApp/service/RemoveFromList.cs
@@ -6,19 +6,32 @@ namespace EngLearningApp
 {
     public static class RemoveFromList
     {
-        private static List<string> affixes = new List<string>(new string[] { "s", "es", "ed", "ing", "\'s", "\'ll", "\n't", "\'t", "\'ve", "\'re" }); //affixes=ragok
+        private static List<string> affixes = new List<string>(new string[] { "s", "es", "ed", "d", "ing", "'s", "'ll", "n't", "'t", "'ve", "'re", "ly" }); //affixes=ragok
 
         public static List<string> affix(List<string> wordList) 
         {
+            wordList.Remove("");
+
             var resultList = new List<string>(wordList);
             foreach (var word in wordList)
             {
                 foreach (var affix in affixes)
                 {
                     if (resultList.Contains(word + affix))
-                        resultList.Remove(word + affix);
+                        resultList.Remove(word + affix);  
                 }
-                
+
+                #region special affix: comE -> comING
+                if (word[word.Length - 1] == 'e')
+                {
+                    //come -> coming
+                    var wordWhitoutLastEPlusIng = word.Substring(0, word.Length - 1) + "ing";
+
+                    if (resultList.Contains(wordWhitoutLastEPlusIng))
+                        resultList.Remove(wordWhitoutLastEPlusIng);
+                }
+                #endregion
+
             }
 
             return resultList;
