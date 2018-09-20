7bb2b47547239168471f8d9d9eba6e1a8db8323a
Balint Sipos <sipos.balint9@gmail.com>
2018-09-20 20:50:43 +0200

    remove unnecessary word.cs constructors

--- EngLearningApp/model/Word.cs
+++ EngLearningApp/model/Word.cs
@@ -12,21 +12,8 @@
     {
         public string english;
         public string hungarian;
-
         public KnownColor knowledgeLevel;
-
-        public Word()
-        {
-
-        }
-
-        public Word(string eng, string hun)
-        {
-            this.english = eng;
-            this.hungarian = hun;
-            this.knowledgeLevel = KnownColor.Red;
-        }
-
+        
         public Word(string eng, string hun,KnownColor color)
         {
             this.english = eng;
